9c913c4f03c761a8ddabe9391404a4fab54a5cf4
Neale Taylor <nealeptaylor@gmail.com>
2022-01-20 15:09:09 -0500

    added fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,3 +9,11 @@ const { fetchMyIP } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
+// fetchCoordsByIP('184.148.118.157', (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , data);
+// });
--- iss.js
+++ iss.js
@@ -14,4 +14,22 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
